a7c2dc66d8603de1e6b0c33db499583c3ccd2bea
Sosha Fedotovskaia <sinterma@gmail.com>
2025-08-25 19:20:15 +0200

    added describtions for tests

--- tests/zalando.spec.ts
+++ tests/zalando.spec.ts
@@ -1,15 +1,18 @@
-import {test, expect} from '@playwright/test'
+import { test, expect } from "@playwright/test";
 
+test.describe("test suit 1", async () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://en.zalando.de");
+  });
 
-test.beforeEach(async ({page}) => {
-    await page.goto('https://en.zalando.de');
-})
+  test("has tittle", async ({ page }) => {
+    await expect(page).toHaveTitle(
+      "Shop Shoes, Fashion & Accessories Online | Zalando"
+    );
+  });
 
-test ('has tittle', async ({page}) => {
-    await expect(page).toHaveTitle('Shop Shoes, Fashion & Accessories Online | Zalando')
-} )
-
-test ('select category women', async ({page}) => {
-    await page.locator('header a[href="/women-home/"]').click()
-    await expect(page).toHaveTitle('Women’s Shoes & Fashion Online | ZALANDO')
-})
+  test("select category women", async ({ page }) => {
+    await page.locator('header a[href="/women-home/"]').click();
+    await expect(page).toHaveTitle("Women’s Shoes & Fashion Online | ZALANDO");
+  });
+});
