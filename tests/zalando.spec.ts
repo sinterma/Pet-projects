d4891eac93baf935c2429369dd3dc4969a11f23b
Sosha Fedotovskaia <sinterma@gmail.com>
2025-08-25 19:12:08 +0200

    added beforeEach

--- tests/zalando.spec.ts
+++ tests/zalando.spec.ts
@@ -1,12 +1,15 @@
 import {test, expect} from '@playwright/test'
 
-test ('has tittle', async ({page}) => {
+
+test.beforeEach(async ({page}) => {
     await page.goto('https://en.zalando.de');
+})
+
+test ('has tittle', async ({page}) => {
     await expect(page).toHaveTitle('Shop Shoes, Fashion & Accessories Online | Zalando')
 } )
 
 test ('select category women', async ({page}) => {
-    await page.goto('https://en.zalando.de')
     await page.locator('header a[href="/women-home/"]').click()
     await expect(page).toHaveTitle('Women’s Shoes & Fashion Online | ZALANDO')
 })
